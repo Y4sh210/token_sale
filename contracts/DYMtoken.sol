52812d2c47eb0eedbf60f3aa15bc8225ef7605de
Y4sh210 <y4shchauhan@gmail.com>
2021-08-04 21:45:06 +0530

    6 Delegate Token Transfer

--- contracts/DYMtoken.sol
+++ contracts/DYMtoken.sol
@@ -8,7 +8,14 @@ contract DYMtoken {
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     constructor(uint256 _initialSupply) public {
         balanceOf[msg.sender] = _initialSupply;
@@ -23,6 +30,31 @@ contract DYMtoken {
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
         emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success)
+    {
+        allowance[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        allowance[_from][msg.sender] -= _value;
+
+        Transfer(_from, _to, _value);
+
         return true;
     }
 }
